626002f8aa6cfea5d75d9b2f776acd4e5c031845
Erik Russell <erik.z.russell@gmail.com>
2024-09-05 00:11:58 -0700

    Working on game logic have a bug that gives the incorrect number of correct numbers out of location

--- lib/game.rb
+++ lib/game.rb
@@ -2,18 +2,39 @@
 
 # Game - logic of game
 class Game
+  attr_reader :code
+
   def initialize
-    # create a new game
-  end
-
-  # computer picks code
-  def new_code
     @code = Array.new(4) { rand(1..6) }
+    @turns = 0
   end
 
-  # play_turn
-  # player guesses code
-  # computer responds with accuracy of guess
-  # count to 12 turns
-  # win or lose
+  def play_turn(guess)
+    puts "for test code:#{@code}"
+    @turns += 1
+
+    correct_position = 0
+    correct_number = 0
+
+    guess.each_with_index do |num, index|
+      if num == @code[index]
+        correct_position += 1
+      elsif @code.include?(num)
+        correct_number += 1
+      end
+    end
+
+    puts "You got #{correct_position} digit(s) in the correct position"
+    puts "You got #{correct_number} digit(s) correct but in the wrong position"
+    puts "You have #{12 - @turns} turns left"
+
+    return true if correct_position == 4
+
+    if @turns >= 12
+      puts "Sorry, you've run out of turns! the code was #{@code.join}."
+      return true
+    end
+
+    false
+  end
 end
--- mastermind.rb
+++ mastermind.rb
@@ -2,9 +2,30 @@
 
 # requirements
 # require 'colorize'
+require_relative 'lib/game'
 
 # initialize game
 
+game = Game.new
+game_over = false
+
 # run game
+until game_over
+  # get guess from player
+  puts 'Enter your guess as a four digit number (e.g. 3246)'
+
+  input = gets.chomp
+
+  # Validate
+  if input.match?(/^\d{4}$/)
+    guess = input.chars.map(&:to_i)
+  else
+    puts 'Invalid input! Please enter only numbers'
+    next
+  end
+
+  game_over = game.play_turn(guess)
+end
 
 # close game
+puts "\t ==== GAME OVER ===="
